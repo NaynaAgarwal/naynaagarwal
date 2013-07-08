178ee4a9403dcd0980815a2b19f4350a11e8b3d4
NaynaAgarwal <nayna1914@gmail.com>
2013-07-08 11:57:17 +0530

    removing demo1 from from master

--- demo1/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
- validates :firstname, :lastname, presence: true
- validates :password, length:{in: 6..40}
-
- before_create do |user|
-  user.username = user.firstname.downcase + "_" + lastname.downcase if user.username.blank?
- end
-end
